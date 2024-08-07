31d53111dc1ed37dc0422dd01997dd414305a2e5
unknown <nilufarsafaly@gmail.com>
2024-08-08 00:48:33 +0400

    Izzi-Services

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
+import data from "./data.json" with { type: "json" }
+console.log(data);
 
-import { app_data } from "./data.js";
-console.log(app_data);
+async function getInfo() {
+    let container = document.querySelector(".container");
+    let services = data.data.services;
 
-async function getInfo(){
-    // let result = await fetch('./data.json').then((res) => res.json());
-    let container = document.querySelector(".container_all");
-    let services = app_data.data.services;
-    for(let i = 0; i < services.length; i++){
+    for (let i = 0; i < services.length; i++) {
         let newService = document.createElement("div");
-        newService.classList.add("container");
-        newService.innerHTML = `
-            <div class="container_image">
-                <img src="${services[i].cover_img.publicUrl}" alt="">
-            </div>
-            <div class="about">
-                <p class="job">${services[i].title_en}</p>
-                <p class="salary"><img src="photos/money.png" alt="">Starts from 11$/h</p>
-            </div>
+        newService.classList.add("card");
+        let image = document.createElement("div");
+        image.classList.add("image");
+        image.style.backgroundImage = `url("${services[i].cover_img.publicUrl}")`;
+        image.innerHTML = `
+          <div class="about">
+            <div class="job">${services[i].title_en}</div>
+            <div class="salary">Starts from 11$/h</div>
+          </div>  
         `;
+        newService.appendChild(image);
 
         let subServices = services[i].subServices;
         let newSubService = document.createElement("div");
-        newSubService.classList.add("details")
+        newSubService.classList.add("details");
         for (let j = 0; j < subServices.length; j++) {
             let newP = document.createElement("p");
-            newP.innerHTML = `
-                ${subServices[j].title_en}            
-            `;
+            newP.innerHTML = `${subServices[j].title_en}`;
             newSubService.appendChild(newP);
         }
         newService.appendChild(newSubService);
         container.appendChild(newService);
     }
+
+    if (window.innerWidth <= 450) {
+        let cards = document.querySelectorAll(".card");
+
+        cards.forEach(function(card) {
+            card.addEventListener("click", function() {
+                let details = card.querySelector(".details");
+                let display = getComputedStyle(details).height !== "0px";
+
+                if (display) {
+                    details.style.height = "0px";
+                } else {
+                    details.style.height = "100px";
+                }
+            });
+        });
+    }
 }
-getInfo();
+
+document.addEventListener("DOMContentLoaded", function() {
+    getInfo();
+});
